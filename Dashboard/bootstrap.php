f8f4ebcc7b18fdd3fe4e3ecbc5439bcd4bac4b8e
Aziz Light <aziiz.light@gmail.com>
2010-09-29 21:21:30 +0200

    Addedd the BASEURL constant.

--- Dashboard/bootstrap.php
+++ Dashboard/bootstrap.php
@@ -3,20 +3,20 @@
 date_default_timezone_set('Europe/Paris');
 
 // -Dubug-Mode-------------------------------------------------------------
-include_once '/Users/aziz/Sources/aziz/git/Foo/Foo.php';  // DEBUG <-
-error_reporting( -1 );
+error_reporting(-1);
 // ------------------------------------------------------------------------
 
 // -Application-Constants--------------------------------------------------
-define( 'APPPATH', dirname( __FILE__ ) );
-define( 'WIDGETDIR', APPPATH . '/Widgets' );
+define('APPPATH', dirname(__FILE__));
+define('WIDGETDIR', APPPATH . '/Widgets');
+define('BASEURL', 'http://localhost/');
 // ------------------------------------------------------------------------
 
 // -Autoloader-------------------------------------------------------------
 require_once APPPATH . '/classes/Autoloaders.php';
 include_once APPPATH . '/classes/Helpers.php';
-spl_autoload_extensions( 'widget=>.php,class=>.class.php' );
-spl_autoload_register( array( 'WidgetAutoloader', 'autoloader' ) );
+spl_autoload_extensions('widget=>.php,class=>.class.php');
+spl_autoload_register(array('WidgetAutoloader', 'autoloader'));
 // ------------------------------------------------------------------------
 
 /* End of file bootstrap.php */
